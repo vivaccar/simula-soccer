aa63dfdf95fe6973b0417b20cd3d988b85b30e4f
Vinicius Vaccari <viniciusvaccarih@hotmail.com>
2024-05-03 20:36:18 +0100

    ordered by points

--- home/views.py
+++ home/views.py
@@ -7,7 +7,7 @@ from .models import Team
 # Create your views here.
 
 def	home(request):
-	teams_list = Team.objects.order_by('points')
+	teams_list = Team.objects.order_by('-points')
 	context = {'teams_list': teams_list}
 	return (render(request, 'home.html', context))
 """ 	template = loader.get_template('home.html')
