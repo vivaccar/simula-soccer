3b84dd1ee3367e763111f5a5dd37992dcb0b7901
Vinicius Vaccari <viniciusvaccarih@hotmail.com>
2024-05-03 17:31:54 +0100

    home.html

--- home/views.py
+++ home/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Team
+
 
 # Create your views here.
 
 def	home(request):
-	template = loader.get_template('home.html')
-	return HttpResponse(template.render())
+	return (render(request, 'home.html'))
+""" 	template = loader.get_template('home.html')
+	return HttpResponse(template.render()) """
