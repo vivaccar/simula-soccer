110e45da47585244875fb73d6cbc3415d4942796
Vinicius Vaccari <viniciusvaccarih@hotmail.com>
2024-05-04 20:12:39 +0100

    submit fail

--- home/forms.py
+++ home/forms.py
@@ -2,6 +2,6 @@ from django import forms
 from django.forms import ModelForm
 from .models import Team
 
-""" class Game(ModelForm):
-	home_goals = forms.IntegerField(label='Gols do Time da Casa')
-	away_goals = forms.IntegerField(label='Gols do Time visitante') """
+class Game_forms(ModelForm):
+	home_goals = forms.IntegerField(label='home_goals')
+	away_goals = forms.IntegerField(label='away_goals')
--- home/views.py
+++ home/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 from .models import Team, Game
+from .forms import Game_forms
 
 	
 # Create your views here.
