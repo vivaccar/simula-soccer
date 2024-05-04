ec09b57fecb74340c52cd7031d27de1b3eae707a
Vinicius Vaccari <viniciusvaccarih@hotmail.com>
2024-05-04 18:37:30 +0100

    grenal

--- home/views.py
+++ home/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
-from .models import Team
-""" from .forms import Game """
 from .models import Team, Game
 
 	
