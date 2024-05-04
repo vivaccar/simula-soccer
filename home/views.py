1b761f849ff8f5752318e931bc97413531bba88d
Vinicius Vaccari <viniciusvaccarih@hotmail.com>
2024-05-04 18:03:51 +0100

    game class created

--- home/views.py
+++ home/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from .models import Team
+from .models import Team, Game
 
 	
 # Create your views here.
 
 def	home(request):
+	games_list = Game.objects.all()
 	teams_list = Team.objects.order_by('-points')
-	context = {'teams_list': teams_list}
+	context = {'teams_list': teams_list, 'games_list': games_list}
 	return (render(request, 'home.html', context))
 """ 	template = loader.get_template('home.html')
 	return HttpResponse(template.render()) """
